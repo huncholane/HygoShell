29f2a43c18fe9cb07e585e66933790707ea1dc05
Lane Henslee <huncho@hygo.ai>
2024-08-07 13:45:51 -0500

    shows the git branch

--- setup.sh
+++ setup.sh
@@ -8,9 +8,20 @@ else
 fi
 cd $CURRENT_DIR
 
+# Function to get the current Git branch
+git_branch_str() {
+    git_branch=$(git branch 2>/dev/null | grep '*' | sed 's/* //')
+    if [ -n "$git_branch" ]; then
+        echo -e "($git_branch)"
+    fi
+}
+
+# Set up zsh prompt
 if [ -n "$ZSH_VERSION" ]; then
     PROMPT="%F{214}%n@%m %F{white}%~"$'\n'"%f$ "
 fi
+
+# Set up bash prompt
 if [ -n "$BASH_VERSION" ]; then
-    PS1="\[\e[38;5;214m\]\u@\h \w\n\$\[\e[0m\] "
+    PS1="\[\e[38;5;214m\]\u@\h \w $(git_branch_str)\n\$\[\e[0m\] "
 fi
